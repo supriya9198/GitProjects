91be1763b57eb9005098934a95e16e3629e27b8d
Supriya <Supriyadeshmukh22416@gmail.com>
2022-12-13 16:20:11 +0530

    Chetan

--- src/main/java/base_pom/Base.java
+++ src/main/java/base_pom/Base.java
@@ -20,6 +20,7 @@ public class Base {
 			Reporter.log("Hello");
 			
 			Reporter.log("rutuja", true);
+			Reporter.log("Chetan", true);
 			Reporter.log("launchng browser", true);
 			Thread.sleep(500);
 			
